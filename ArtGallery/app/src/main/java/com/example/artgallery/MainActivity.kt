b8b392eb9a9ae8fffde7ced6e641813641016a78
Jsk1n22 <jsk1n22@soton.ac.uk>
2024-07-22 13:29:20 +0100

    Add dynamic image changing

--- ArtGallery/app/src/main/java/com/example/artgallery/MainActivity.kt
+++ ArtGallery/app/src/main/java/com/example/artgallery/MainActivity.kt
@@ -8,9 +8,12 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.artgallery.ui.theme.ArtGalleryTheme
 
 class MainActivity : ComponentActivity() {
@@ -95,7 +99,11 @@ fun ArtGallery(
             year = year ,
             modifier = modifier
         )
-
+        DisplayController(
+            onNextChange = {artworkNum++},
+            onPrevChange = {artworkNum--},
+            modifier = modifier
+        )
     }
 }
 
@@ -125,6 +133,23 @@ fun ArtworkDescription(
         text = "$artist ($year)",
         modifier = modifier
     )
+}
+
+@Composable
+fun DisplayController(
+    onNextChange: () -> Unit,
+    onPrevChange: () -> Unit,
+    modifier: Modifier
+) {
+    Row {
+        Button(onClick = onPrevChange) {
+            Text(text = "Previous")
+        }
+        Spacer(modifier = Modifier.padding(10.dp))
+        Button(onClick = onNextChange) {
+            Text(text = "Next")
+        }
+    }
 }
 
 @Preview(
