ce616f03d39343c8dcde12176caba83a36b14d7f
Jsk1n22 <jsk1n22@soton.ac.uk>
2024-08-22 15:47:26 +0100

    Change to LazyVerticalGrid object and add icon

--- Courses/app/src/main/java/com/example/courses/MainActivity.kt
+++ Courses/app/src/main/java/com/example/courses/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
@@ -17,17 +18,23 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.courses.data.DataSource
@@ -67,13 +74,21 @@ fun TopicCard(
                 Text (
                     text = stringResource(id = topic.name),
                     style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 8.dp)
+                    modifier = Modifier.padding(top = 16.dp, start = 16.dp, bottom = 8.dp)
                 )
-                Text(
-                    text = topic.count.toString(),
-                    style = MaterialTheme.typography.labelMedium,
-                    modifier = Modifier.padding(start = 16.dp)
-                )
+
+                Row (
+                    modifier = Modifier.padding(start = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(painterResource(id = R.drawable.ic_grain), contentDescription = null)
+                    Text (
+                        text = topic.count.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                }
+
             }
         }
     }
@@ -83,8 +98,6 @@ fun TopicCard(
 fun CoursesApp() {
     val layoutDirection = LocalLayoutDirection.current
     val topicList = DataSource.topics
-    val firstHalf = topicList.take(topicList.size/2)
-    val secondHalf = topicList.drop(topicList.size/2)
     Surface(
         modifier = Modifier
             .fillMaxSize()
@@ -99,8 +112,7 @@ fun CoursesApp() {
             ),
     ) {
         Row {
-            CoursesList(firstHalf)
-            CoursesList(secondHalf)
+            CoursesList(topicList)
         }
     }
 }
@@ -110,9 +122,12 @@ fun CoursesList (
     topicList: List<Topic>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
-        modifier = modifier
-    ) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier)
+    {
         items(topicList) { a ->
             TopicCard(
                 topic = a,
